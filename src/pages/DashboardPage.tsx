31aa93ededf88b5ef5ef618489779db4cf35c8a4
GimK <74003102+Gimsara-kenula@users.noreply.github.com>
2026-05-01 01:17:40 +0800

    Added Readme file

--- src/pages/DashboardPage.tsx
+++ src/pages/DashboardPage.tsx
@@ -773,7 +773,7 @@ export function DashboardPage() {
           initial={{ opacity: 0, x: 20 }}
           animate={{ opacity: 1, x: 0 }}
           transition={{ duration: 0.6, delay: 0.25 }}
-          className="w-full lg:w-72 xl:w-80 shrink-0 h-full"
+          className="w-full lg:w-72 xl:w-80 shrink-0"
         >
           <MealPlannerPanel caregiverId={caregiverId} navigate={navigate} onSaved={refreshAgenda} />
         </motion.div>
